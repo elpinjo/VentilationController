c9e1cc0377fb84da6eefc876c9f97e648b80289e
Heuveling <mihe@softwareag.com>
2020-01-28 18:03:25 +0100

    Add config manager in startup of app

--- src/application/core.cpp
+++ src/application/core.cpp
@@ -1,12 +1,15 @@
 #include "core.hpp"
 #include "../ota/ota.hpp"
 #include "../network/wlan.hpp"
+#include "../config/config.hpp"
 
 ota otaManager = ota();
 wlan wlanManager = wlan();
+config configManager = config();
 
 void core::init() {
 
+    configManager.init();
     wlanManager.init();
     otaManager.init();
 }
